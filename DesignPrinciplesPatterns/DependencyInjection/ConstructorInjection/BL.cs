f5bfe697019e12418ef31805812aa8aeb6bfb331
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2020-12-30 17:51:27 +0530

    Some more updates

---
+++ DesignPrinciplesPatterns/DependencyInjection/ConstructorInjection/BL.cs
@@ -0,0 +1,18 @@
+﻿using DependencyInversionPrinciple;
+
+namespace DependencyInjection.ConstructorInjection
+{
+    class BL
+    {
+        IDAL mDal;
+        public BL(IDAL dal)
+        {
+            mDal = dal;
+        }
+
+        public string GetCustomerName(int id)
+        {
+            return mDal.GetCustomerName(id);
+        }
+    }
+}
---
+++ DesignPrinciplesPatterns/DependencyInjection/MethodInjection/BL.cs
@@ -0,0 +1,18 @@
+﻿using DependencyInversionPrinciple;
+
+namespace Example
+{
+    class BL
+    {
+        IDAL mDal;
+        public void Initialize(IDAL dal)
+        {
+            mDal = dal;
+        }
+
+        public string GetCustomerName(int id)
+        {
+            return mDal.GetCustomerName(id);
+        }
+    }
+}
