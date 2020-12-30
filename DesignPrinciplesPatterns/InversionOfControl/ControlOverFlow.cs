35780536dffb400f6baed666fee62dfdb1b10241
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2020-12-30 16:23:30 +0530

    This has IoC demonstrated

---
+++ DesignPrinciplesPatterns/InversionOfControl/ControlOverFlow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InversionOfControl
+{
+    public class ControlOverFlow
+    {
+        /// <summary>
+        /// Main() method controls the flow  of execution here
+        /// If we introduce a GUI then the control will be handled by
+        /// or taken care by that itself
+        /// </summary>
+        void main()
+        {
+            SomeExecution();
+        }
+
+        public void SomeExecution()
+        {
+            bool continuee = true;
+            while (continuee)
+            {
+
+                DoTask1();
+                DoTask2();
+
+                continuee = ShouldContinue();
+            }
+        }
+
+        private bool ShouldContinue()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoTask2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DoTask1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
